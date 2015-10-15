4cdb3fb161cf740f3ff13aa9e3d1085e85b729a1
Jonathan Eskew <eskewj@amazon.com>
2015-10-15 12:08:53 -0700

    Immediately resolve each promises that are passed an empty iterator

--- src/EachPromise.php
+++ src/EachPromise.php
@@ -83,6 +83,11 @@ class EachPromise implements PromisorInterface
     {
         $this->aggregate = new Promise(function () {
             reset($this->pending);
+            if (empty($this->pending) && !$this->iterable->valid()) {
+                $this->aggregate->resolve(null);
+                return;
+            }
+
             // Consume a potentially fluctuating list of promises while
             // ensuring that indexes are maintained (precluding array_shift).
             while ($promise = current($this->pending)) {
