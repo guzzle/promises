5ec5c7b93753b8f0e25311f20b0885c784dcc011
Nicolas Grekas <nicolas.grekas@gmail.com>
2024-03-20 16:08:44 +0100

    Make implicit nullable types explicit

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,6 +10,7 @@ $config = (new PhpCsFixer\Config())
         'no_superfluous_phpdoc_tags' => [
             'allow_mixed' => true,
         ],
+        'nullable_type_declaration_for_default_null_value' => true,
         'phpdoc_annotation_without_dot' => false,
         'phpdoc_summary' => false,
         'phpdoc_to_comment' => false,
